22cd5605712e34b4fb9106f749c3bda06106c211
stau04@gmail.com <stau04@gmail.com>
2022-11-08 02:47:48 +0900

    aws ssl 연결 테스트2

--- src/main/java/com/app/thingsilove/web/user/HomeController.java
+++ src/main/java/com/app/thingsilove/web/user/HomeController.java
@@ -6,18 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/health")
-    public ResponseEntity healthCheck(){
+    public ResponseEntity<String> healthCheck(HttpServletResponse response){
         System.out.println("aws health check");
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,"*");
         headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,"*");
         headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS,"true");
         headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,"*");
-        return ResponseEntity.ok().headers(headers).body("");
+        return ResponseEntity.status(200).headers(headers).body("");
     }
     @GetMapping("/")
     public String sessionExpired(){
