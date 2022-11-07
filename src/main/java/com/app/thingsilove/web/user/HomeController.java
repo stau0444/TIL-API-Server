f4a7b3156a8ac7f684fdbb1770e5e808b6ceaef6
stau04@gmail.com <stau04@gmail.com>
2022-11-08 01:30:31 +0900

    /health 핸들러 추가

--- src/main/java/com/app/thingsilove/web/user/HomeController.java
+++ src/main/java/com/app/thingsilove/web/user/HomeController.java
@@ -1,12 +1,15 @@
 package com.app.thingsilove.web.user;
 
 import com.app.thingsilove.web.common.security.InvalidSessionException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @GetMapping("/health")
+    public ResponseEntity healthCheck(){return ResponseEntity.ok().body(null);}
     @GetMapping("/")
     public String sessionExpired(){
         throw new InvalidSessionException();
